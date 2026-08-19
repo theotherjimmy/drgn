67e9f12be47f03b3719509bf9a33301b04cd4d41
Jimmy Brisson <jbrisson@linux.ibm.com>
2026-08-18 21:04:00 -0500

    PPC64: Retrieve radix mmu enabled bit from vmcoreinfo
    An affect of checking that we're using an mmu with radix mode enabled
    is that it reads from kernel structures and therefore will try to do
    address translation to check this. Since this is checked during
    arch-specific address translation initialization, this may have the
    affect of trying to do address translation during address translation
    initialization, which would clearly trigger the address translation
    recursion logic.
    
    Luckily, this is stored in the VMCOREINFO, and we can avoid recursive
    address translation by parsing the RADIX_MMU bit from there and skip
    reading through kernel structures when we can grab it from vmcoreinfo.
    
    Signed-off-by: Jimmy Brisson <jbrisson@linux.ibm.com>

--- libdrgn/arch_ppc64.c
+++ libdrgn/arch_ppc64.c
@@ -326,6 +326,33 @@ get_index(struct pgtable_iterator_ppc64 *it_arch, uint64_t va, uint16_t level)
 }
 
 static struct drgn_error *
+fail_if_not_radix_enabled(struct drgn_program * prog)
+{
+	if (prog->vmcoreinfo.ppc64_radix_mmu)
+	    return NULL;
+
+	struct drgn_error *err = NULL;
+	DRGN_OBJECT(tmp, prog);
+	// Identify the MMU type.
+	err = drgn_program_find_object(prog, "cur_cpu_spec", NULL,
+	                               DRGN_FIND_OBJECT_ANY, &tmp);
+	if (err)
+	        return err;
+	err = drgn_object_member_dereference(&tmp, &tmp, "mmu_features");
+	if (err)
+	        return err;
+	uint64_t mmu_features;
+	err = drgn_object_read_unsigned(&tmp, &mmu_features);
+
+	if (err)
+	        return err;
+	if (!(mmu_features & 0x40)) {
+	        return drgn_error_create(DRGN_ERROR_NOT_IMPLEMENTED,
+	                                 "virtual address translation is only supported for Radix MMU");
+	}
+}
+
+static struct drgn_error *
 linux_kernel_pgtable_iterator_arch_create_ppc64(struct drgn_program * prog,
 						void **ret)
 {
@@ -356,23 +383,9 @@ linux_kernel_pgtable_iterator_arch_create_ppc64(struct drgn_program * prog,
 	if (!drgn_error_catch(&err, DRGN_ERROR_LOOKUP))
 		return err;
 
-	// Identify the MMU type.
-	err = drgn_program_find_object(prog, "cur_cpu_spec", NULL,
-				       DRGN_FIND_OBJECT_ANY, &tmp);
+	err = fail_if_not_radix_enabled(prog);
 	if (err)
 		return err;
-	err = drgn_object_member_dereference(&tmp, &tmp, "mmu_features");
-	if (err)
-		return err;
-	uint64_t mmu_features;
-	err = drgn_object_read_unsigned(&tmp, &mmu_features);
-
-	if (err)
-		return err;
-	if (!(mmu_features & 0x40)) {
-		return drgn_error_create(DRGN_ERROR_NOT_IMPLEMENTED,
-					 "virtual address translation is only supported for Radix MMU");
-	}
 
 	*ret = no_cleanup_ptr(it_arch);
 	return NULL;
--- libdrgn/program.h
+++ libdrgn/program.h
@@ -232,6 +232,8 @@ struct drgn_program {
 				 * Whether 5-level paging was enabled on x86-64.
 				 */
 				bool pgtable_l5_enabled;
+				/** Whether the PPC64 mmu is configured to Radix mode */
+				bool ppc64_radix_mmu;
 				/** Whether LPAE was enabled on Arm. */
 				bool arm_lpae;
 				/** Whether `CRASHTIME` was in the VMCOREINFO. */
